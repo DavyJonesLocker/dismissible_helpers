90002a08a62677f08d2611ccee907b4ebfef2235
Dan McClain <git@danmcclain.net>
2012-12-29 08:12:38 -0500

    Removes 1.9 hash style

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -3,7 +3,7 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 Bundler.require
-require "dismissible_helpers"
+require 'dismissible_helpers'
 
 module Dummy
   class Application < Rails::Application
