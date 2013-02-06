3056b41a3ae1e7f5a2830abd86ab9e0cedbd05e8
Ramon Tayag <ramon@tayag.net>
2013-02-06 22:43:48 +0800

    Refactor dismissed helper checking to smaller classes

--- lib/dismissible_helpers.rb
+++ lib/dismissible_helpers.rb
@@ -7,6 +7,9 @@ module DismissibleHelpers
   autoload :Routes
   autoload :Helpers
   autoload :ContentBuilder
+  autoload :DismissedChecker
+  autoload :DismissedHelpersExtractor
+
 end
 
 ActionDispatch::Routing::Mapper.send(:include, DismissibleHelpers::Routes)
--- lib/dismissible_helpers/helpers/dismissible_helpers.rb
+++ lib/dismissible_helpers/helpers/dismissible_helpers.rb
@@ -1,22 +1,19 @@
 module DismissibleHelpers::Helpers::DismissibleHelpers
+
   def render_dismissible_helper(name)
-    unless has_been_dismissed?(name)
+    unless dismissed?(name)
       DismissibleHelpers::ContentBuilder.build(name)
     end
   end
 
   private
 
-  def has_been_dismissed?(name)
-    if respond_to?(:current_user) && current_user && current_user.respond_to?(:dismissed_helpers)
-      dismissed_helpers = current_user.dismissed_helpers
-    else
-      if cookies[:dismissed_helpers].present?
-        dismissed_helpers = cookies[:dismissed_helpers].split '|'
-      else
-        dismissed_helpers = []
-      end
-    end
-    dismissed_helpers.include? name
+  def dismissed?(name)
+    DismissibleHelpers::DismissedChecker.dismissed?(
+      name,
+      current_user,
+      cookies[:dismissed_helpers]
+    )
   end
+
 end
